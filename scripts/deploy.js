a4e73caf73a48eb8faf554a66ddc8687bf110639
Pablo Santos <iphonepablosantos@gmail.com>
2024-11-03 19:29:26 +0000

    voteSystem

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const currentTimestamp = Math.floor(Date.now() / 1000); // timestamp atual em segundos
   const votingStart = currentTimestamp;
-  const votingEnd = currentTimestamp + 60; // 7 dias a partir de agora
+  const votingEnd = currentTimestamp + (7 * 24 * 60 * 60); // 7 dias a partir de agora
 
   const VotingSystem = await hre.ethers.getContractFactory("VotingSystem");
   const votingSystem = await VotingSystem.deploy(votingStart, votingEnd);
